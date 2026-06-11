3de31b4ea3b437c440fc78e5e692b86012e6d3c4
Taka <amano.takahisa@gmail.com>
2026-06-11 18:35:58 +0900

    remove broken webUI workaround

--- modules/darwin.nix
+++ modules/darwin.nix
@@ -7,23 +7,6 @@
 
   # allow unfree packages（VS Code, etc.）
   nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.allowBroken = true;
-
-  # fix broken dlinfo dependencies of open-webuidlinfo
-  # test of dlinfo requires libdl.dylib which is not available on macOS.
-  # This can be removed if upstream is fixed: https://github.com/NixOS/nixpkgs/issues
-  nixpkgs.overlays = [
-    (final: prev: {
-      python313 = prev.python313.override {
-        packageOverrides = _: pyPrev: {
-          dlinfo = pyPrev.dlinfo.overridePythonAttrs (_: {
-            doCheck = false;
-            meta = pyPrev.dlinfo.meta // { broken = false; };
-          });
-        };
-      };
-    })
-  ];
 
   # Tell user info to nix-darwin
   users.users.Chie = {
