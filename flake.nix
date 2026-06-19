28ea6963c187c06e23d2720dad00783dc7bfa0b7
Chie <93822689+chie-amano@users.noreply.github.com>
2026-06-19 22:10:58 +0900

    Fix nix-darwin branch to master (26.11 not released yet)

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-26.11-darwin";
     nix-darwin = {
-      url = "github:nix-darwin/nix-darwin/nix-darwin-26.11";
+      url = "github:nix-darwin/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
