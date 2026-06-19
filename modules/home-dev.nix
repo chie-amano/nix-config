f5d6cabf6f34198cbcfc1851d06c27eeb3b6069a
Taka <amano.takahisa@gmail.com>
2026-06-20 07:01:55 +0900

    update git config
    add aliases

--- modules/home-dev.nix
+++ modules/home-dev.nix
@@ -7,6 +7,17 @@
   programs.git = {
     enable = true;
     settings = {
+      alias = {
+        aliases = "config --get-regexp '^alias\\.'";
+        bl = "blame --abbrev=6";
+        lo = "log --graph --all --date=format:'%Y-%m-%d %H:%M' --format='%C(white dim) %h %Creset %s %C(cyan dim)(%ad)%Creset%C(green) <%an>%C(bold yellow)%d%Creset'";
+        loo = "log --stat --graph --decorate --all";
+        pushf = "push --force-with-lease --force-if-includes";
+        root = "rev-parse --show-toplevel";
+        sh = "show --color-words='[^[:space:]]'";
+        st = "status --short --branch";
+        wt = "!f() { if [ -z \"$1\" ]; then echo \"Create a new branch as a worktree.\nUsage:\n  $ git wt <new-branch-name>\"; return 1; fi; ROOT=\"$(git rev-parse --show-toplevel)\"; WORKTREE_PATH=\"$(dirname \"$ROOT\")/$(basename \"$ROOT\")=$1\"; git worktree add --quiet \"$WORKTREE_PATH\" -b \"$1\" && echo \"$WORKTREE_PATH\"; }; f";
+      };
       commit = { verbose = "true"; };
       fetch = { prune = "true"; };
       ghq.root = "~/ghq";
