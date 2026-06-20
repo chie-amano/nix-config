09b94573170933287d2ab3683b082b6687ea5710
Taka <amano.takahisa@gmail.com>
2026-06-20 09:18:20 +0900

    ghostty and fish

--- modules/home-personal.nix
+++ modules/home-personal.nix
@@ -87,4 +87,19 @@
     enable = true;
     version.enableNixpkgsReleaseCheck = false;
   };
+
+  programs.fish.enable = true;
+
+  programs.ghostty = {
+    enable = true;
+    enableFishIntegration = true;
+    settings = {
+      # ctrl + shift + , to reload ghostty settings.
+      # https://ghostty.org/docs/config/reference
+      adjust-cursor-thickness = 6;
+      theme = "dark";
+      command = "fish";
+    };
+  };
+
 }
